f024fb0a82b1c66b9966d1e55c0359c1d3bab2be
HappyRichLove <sergpolik@gmail.com>
2026-04-10 23:28:21 +0500

    v19.11: ❤️ MASTER BREATH🙏

---
+++ sw.js
@@ -0,0 +1,22 @@
+var CACHE = 'master-v19.8';
+var FILES = ['./index.html', './favicon.svg', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.filter(function(n) { return n !== CACHE; }).map(function(n) { return caches.delete(n); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
